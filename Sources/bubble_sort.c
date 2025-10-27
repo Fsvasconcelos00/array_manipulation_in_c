44d83f803fd39f8469234d19171657988735d8e3
FsVasconcelos00 <fsv00@proton.me>
2025-10-26 22:18:05 -0300

    Adding visualization to the sorting algorithms

--- Sources/bubble_sort.c
+++ Sources/bubble_sort.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "bubble_sort.h"
 
 void bubble_sort(arrayConfig_t my_array)
 {
+    printf("Sorting array:\n");
     for(uint32_t i = 0; i < (my_array.array_size - 1); i++)
     {
         for(uint32_t j = 0; j < (my_array.array_size - 1 - i); j++)
@@ -14,6 +16,15 @@ void bubble_sort(arrayConfig_t my_array)
                 my_array.array[j] = my_array.array[j + 1];
                 my_array.array[j + 1] = aux;
             }
+            printf("\r[");
+            for(uint32_t k = 0; k < my_array.array_size; k++)
+            {
+                printf("%d ", my_array.array[k]);
+            }
+            printf("]");
+            fflush(stdout);
+            usleep(75000);
         }
     }
+    printf("\n");
 }
--- Sources/selection_sort.c
+++ Sources/selection_sort.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "selection_sort.h"
 
 void selection_sort(arrayConfig_t my_array)
 {
+    printf("Sorting array:");
     for(uint32_t i = 0; i < my_array.array_size; i++)
     {
         for(uint32_t j = i; j < my_array.array_size; j++)
@@ -14,6 +16,16 @@ void selection_sort(arrayConfig_t my_array)
                 my_array.array[i] = my_array.array[j];
                 my_array.array[j] = aux;
             }
+            printf("\r[");
+
+            for(uint32_t k = 0; k < my_array.array_size; k++)
+            {
+                printf("%d ", my_array.array[k]);
+            }
+            printf("]");
+            fflush(stdout);
+            usleep(75000);
         }
     }
+    printf("\n");
 }
